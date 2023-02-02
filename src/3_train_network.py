dfe1ba940ced7bedfd202e2d27e897f4301eb134
Pablo RP <pablorodriper@gmail.com>
2023-02-02 16:21:23 -0300

    Build model

--- src/3_train_network.py
+++ src/3_train_network.py
@@ -1,5 +1,11 @@
 import pickle
 
+import numpy as np
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.layers import Conv2D, Dense, Flatten, MaxPooling2D
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import SGD
+
 def load_data():
     """
     Load data from pickle file
@@ -21,10 +27,35 @@ def cast_and_normalize_images(train, test):
     return train_norm, test_norm
 
 
+def get_model():
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', input_shape=(28, 28, 1)))
+    model.add(MaxPooling2D((2, 2)))
+    # model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform'))
+    # model.add(MaxPooling2D((2, 2)))
+    model.add(Flatten())
+    model.add(Dense(100, activation='relu', kernel_initializer='he_uniform'))
+    model.add(Dense(5, activation='softmax'))
+    # compile model
+    opt = SGD(learning_rate=0.01, momentum=0.9)
+    model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
+
+    return model
+
+
 if __name__ == '__main__':
     train_x, train_y, test_x, test_y = load_data()
     train_x, test_x = cast_and_normalize_images(train_x, test_x)
 
+    train_x, val_x, train_y, val_y = train_test_split(
+        train_x, 
+        train_y,
+        test_size=0.2, 
+        shuffle=True,
+        random_state=42,
+    )
+
+    model = get_model()
 
 
 
