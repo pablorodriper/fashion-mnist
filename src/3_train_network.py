db5922321598a0d62ac5a4533c034f37bc98460d
Pablo RP <pablorodriper@gmail.com>
2023-02-02 16:19:16 -0300

    Cast and normalize data

--- src/3_train_network.py
+++ src/3_train_network.py
@@ -1,13 +1,35 @@
 import pickle
 
 def load_data():
-    # Load data
+    """
+    Load data from pickle file
+    """
     with open('../data/processed/fashion_mnist_k5.pkl', 'rb') as f:
         (train_x, train_y), (test_x, test_y) = pickle.load(f)
         return train_x, train_y, test_x, test_y
 
+def cast_and_normalize_images(train, test):
+    """
+    Convert from integers to floats and normalize to range 0-1
+    """
+    train_norm = train.astype('float32')
+    test_norm = test.astype('float32')
+    
+    train_norm = train_norm / 255.0
+    test_norm = test_norm / 255.0
+    
+    return train_norm, test_norm
+
+
 if __name__ == '__main__':
     train_x, train_y, test_x, test_y = load_data()
+    train_x, test_x = cast_and_normalize_images(train_x, test_x)
+
+
+
+
+
+
 
 
 
