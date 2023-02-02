a0e7ead11f992ce45eb91d802246957aa3d66486
Pablo RP <pablorodriper@gmail.com>
2023-02-02 17:09:17 -0300

    Move load_keras_model() to utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import pickle
+from tensorflow.keras.models import load_model
 
 def load_data():
     """
@@ -7,3 +8,10 @@ def load_data():
     with open('../data/processed/fashion_mnist_k5.pkl', 'rb') as f:
         (train_x, train_y), (test_x, test_y) = pickle.load(f)
         return train_x, train_y, test_x, test_y
+
+
+def load_keras_model():
+    """
+    Load keras model from h5 file
+    """
+    return load_model('../models/keras_model.h5')
